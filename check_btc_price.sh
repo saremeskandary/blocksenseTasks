71b4ad9cb0e791161f78d6d2f560bcab2560c66a
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2024-10-19 12:15:54 +0300

    feat(config_files): Single anvil and upgraded configs

---
+++ check_btc_price.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONTACT=$(docker compose logs scdeploy  | grep 'UpgradeableProxy' | awk '{print $NF}')
+echo "UpgradeableProxy contract address $CONTACT"
+
+ANVIL_CONTAINER=$(docker ps | grep foundry | awk '{ print $1}')
+echo "Anvil container id $ANVIL_CONTAINER"
+
+BTC_FEED_ID="1f" # 31 feed id in HEX
+echo "BTC Price from contarct"
+docker exec -it $ANVIL_CONTAINER sh -c "cast call ${CONTACT} --data 0x800000${BTC_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 | cast to-dec" | awk '{print $1*1e-18}'
