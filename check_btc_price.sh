d541feda418db756ee3287c60d56ac1d450752a0
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2024-10-23 20:53:42 +0300

    minor fixes

--- check_btc_price.sh
+++ check_btc_price.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTACT=$(docker compose logs scdeploy  | grep 'UpgradeableProxy' | awk '{print $NF}')
+CONTACT=$(docker compose logs scdeploy  | grep 'UpgradeableProxy' | head -n 1 | awk '{print $NF}')
 echo "UpgradeableProxy contract address $CONTACT"
 
 ANVIL_CONTAINER=$(docker ps | grep foundry | awk '{ print $1}')
@@ -8,7 +8,7 @@ echo "Anvil container id $ANVIL_CONTAINER"
 
 BTC_FEED_ID="1f" # 31 feed id in HEX
 echo "BTC Price from contract"
-docker exec -it $ANVIL_CONTAINER sh -c "cast call ${CONTACT} --data 0x800000${BTC_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 | cast to-dec" | awk '{print $1*1e-18}' && echo '' 
+docker exec -it ${ANVIL_CONTAINER} sh -c "cast call ${CONTACT} --data 0x800000${BTC_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 | cast to-dec" | awk '{print ($1 * 1e-18) }' && echo '' 
 
 TEXT_FEED_ID="de" # 222 feed id in HEX
 echo "Raw bytes from contract"
