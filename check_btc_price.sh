c5f17f55afa7d671c84b6375f650c10cbbbe91dc
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2024-10-23 11:23:36 +0300

    fix typo

--- check_btc_price.sh
+++ check_btc_price.sh
@@ -7,11 +7,11 @@ ANVIL_CONTAINER=$(docker ps | grep foundry | awk '{ print $1}')
 echo "Anvil container id $ANVIL_CONTAINER"
 
 BTC_FEED_ID="1f" # 31 feed id in HEX
-echo "BTC Price from contarct"
+echo "BTC Price from contract"
 docker exec -it $ANVIL_CONTAINER sh -c "cast call ${CONTACT} --data 0x800000${BTC_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 | cast to-dec" | awk '{print $1*1e-18}' && echo '' 
 
 TEXT_FEED_ID="de" # 222 feed id in HEX
-echo "Raw bytes from contarct"
+echo "Raw bytes from contract"
 docker exec -it $ANVIL_CONTAINER sh -c "cast call ${CONTACT} --data 0x800000${TEXT_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 " && echo '' 
-echo "Text from contarct"
+echo "Text from contract"
 docker exec -it $ANVIL_CONTAINER sh -c "cast call ${CONTACT} --data 0x800000${TEXT_FEED_ID} --rpc-url http://127.0.0.1:8545 |  cut -c1-50 " |  xxd -r -p && echo '' 
